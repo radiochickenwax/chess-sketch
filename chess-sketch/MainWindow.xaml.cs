8a8c9005fae440c8fe391246b5b495c0bef5d84b
gregorio.jp.mitchell@gmail.com <gregorio.jp.mitchell@gmail.com>
2018-11-24 14:57:57 -0700

    Switching to a regular grid from a uniform grid

--- chess-sketch/MainWindow.xaml.cs
+++ chess-sketch/MainWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace chess_sketch
             InitializeComponent();
             InitializeChessboard();
             FillInitializedChessboard();
+            // FillInitialPieces();
             //for (int i = 0; i < 10; i++)
             //{
             //    ColumnDefinition c1 = new ColumnDefinition();
@@ -45,17 +46,32 @@ namespace chess_sketch
             //}
         }
 
+        private void FillInitialPieces()
+        {
+            Image pawnImage = new Image();
+            pawnImage.Width = 100;
+            pawnImage.Height = 100;
+            ImageSource pawn = new BitmapImage
+            {
+                UriSource = new Uri("/images/3d-wood/bp.png", UriKind.Relative)
+            };
+            pawnImage.Source = pawn;
+            //Grid.SetRow(pawnImage, 0);
+            //Grid.SetColumn(pawnImage, 0);
+            MainGrid.Children.Add(pawnImage);
+        }
+
         private void InitializeChessboard()
         {
             //GridLengthConverter myGridLengthConverter = new GridLengthConverter();
             //GridLength side = (GridLength)myGridLengthConverter.ConvertFromString("2*");
-            //for (int i = 0; i < ; i++)
-            //{
-            //    MainGrid.ColumnDefinitions.Add(new ColumnDefinition());
-            //    MainGrid.ColumnDefinitions[i].Width = side;
-            //    MainGrid.RowDefinitions.Add(new RowDefinition());
-            //    MainGrid.RowDefinitions[i].Height = side;
-            //}
+            for (int i = 0; i < 8; i++)
+            {
+                MainGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                // MainGrid.ColumnDefinitions[i].Width = side;
+                MainGrid.RowDefinitions.Add(new RowDefinition());
+                // MainGrid.RowDefinitions[i].Height = side;
+            }
 
         }
 
@@ -63,26 +79,26 @@ namespace chess_sketch
         {
             //int squareSize = Math.Max(
             //        (double)(Window.WidthProperty) / 8,
-            //        (double)( Window.HeightProperty*0.125) );
+            //        (double)(Window.HeightProperty * 0.125));
             //int squareSize = 50;
             //var a = Window.WidthProperty;
             //Rectangle[,] square = new Rectangle[9, 9];
-            
+
             for (int row = 0; row < 8; row++)
                 for (int col = 0; col < 8; col++)
                 {
                     Border b = new Border();
-                    
+
                     Label square = new Label();
-                    square.Content = row.ToString()+","+col.ToString();
-                    
-                    Grid.SetColumn(square, col);
-                    Grid.SetRow(square, row);
+                    square.Content = row.ToString() + "," + col.ToString();
+
+                    Grid.SetColumn(b, col);
+                    Grid.SetRow(b, row);
                     if ((row + col) % 2 == 0)
                     {
                         //square[row, col].Fill = new SolidColorBrush(System.Windows.Media.Color.FromRgb(255, 255, 255));
                         b.Background = new SolidColorBrush(System.Windows.Media.Color.FromRgb(255, 255, 255));
-                        square.Foreground = new SolidColorBrush(System.Windows.Media.Color.FromRgb(0,0,0));
+                        square.Foreground = new SolidColorBrush(System.Windows.Media.Color.FromRgb(0, 0, 0));
                     }
                     else
                     {
